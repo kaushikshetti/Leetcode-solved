d85999a789c1a0f2197cf11c20d8cee8906b9ade
Kaushik Shetti <kshetti29@gmail.com>
2023-03-10 15:27:00 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,7 +1,6 @@
-console.log("Hello world");
 function binarySearch(arr:number[],value:number,start:number,end:number): number{
-    while(start<=end){
-        const mid=(start+end)/2;
+    if(start<=end){
+        let mid=start + Math.floor((end - start) / 2);
         if(arr[mid]===value){
             return mid;
         }
@@ -9,15 +8,15 @@ function binarySearch(arr:number[],value:number,start:number,end:number): number
             return binarySearch(arr,value,mid+1,end);
         }
         else{
-             return binarySearch(arr,value,start,mid-1);
+            return binarySearch(arr,value,start,mid-1);
         }
     }
     return -1
 
 }
 
-const arr=[1,2,3];
-const p=1;
+const arr=[1,2,3,4];
+const p=3;
 let n=arr.length-1;
 const index=binarySearch(arr,p,0,n);
 console.log("The position of the element is",index+1);
