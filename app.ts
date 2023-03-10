5d0254c6091c068f1613c1218a26a3913b5d732d
Kaushik Shetti <kshetti29@gmail.com>
2023-03-10 11:08:27 +0530

    Linear Search implemented

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
 console.log("Hello world");
+function linearSearch(arr: number[], value: number) {
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]==value){
+            return i
+        }
+    }
+    return -1;
+}
+
+const arr=[5,6,7,8]
+const index=linearSearch(arr,8)
+console.log("The element is found at position",index);
